85d3c345fd72823f5a5129e8fe6140f4c11e288b
Tae Hwan Jung <77234581+mksymphony@users.noreply.github.com>
2023-08-15 22:55:46 +0900

    Grid_Test

--- Assets/Script/Player/Commander/PlacementSystem.cs
+++ Assets/Script/Player/Commander/PlacementSystem.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField] private GameObject _mouseIndicator;
+    [SerializeField] private GameObject _cellIndicator;
+
     [SerializeField] private CommanderManager _inputManager;
+    [SerializeField] private Grid _grid;
 
     private void Awake()
     {
@@ -18,7 +21,10 @@ public class PlacementSystem : MonoBehaviour
     private void Update()
     {
         Vector3 mousePosition = _inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = _grid.WorldToCell(mousePosition);
+
         _mouseIndicator.transform.position = mousePosition;
+        _cellIndicator.transform.position = _grid.CellToWorld(gridPosition);
     }
 
 }
